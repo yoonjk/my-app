7c030a2acd9b46dc2fad9b73b8139cd62f11a632
Student User <student@workstation.lab.example.com>
2022-06-28 05:49:14 -0400

    Develop app using volume mount

--- hello-nodejs/app.js
+++ hello-nodejs/app.js
@@ -1,8 +1,29 @@
 var express = require('express');
+var fs = require('fs');
+
+const swaggerUI  = require('swagger-ui-express');
+const swaggerDoc = require('./swagger.json');
+
 app = express();
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
+var msg = process.env.APP_MSG;
 
 app.get('/', function (req, res) {
   res.send('Simple app  version 2 for the Building Applications Lab!\n');
+});
+
+app.get('/mounts/:key', function (req, res) {
+  let response = '';
+  fs.readFile('/opt/app-root/' + req.params.key, 'utf8', (err, line)=> {
+    if (err) {
+      console.log(err + '\n');
+      response += err + '\n';
+    }
+    else {
+      response += line + '\n';
+    }
+    res.send(response);
+  });
 });
 
 app.listen(8080, function () {
