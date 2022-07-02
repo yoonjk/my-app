22a8b5ef786a06b912e3fa2119373a2086b005ee
Student User <student@workstation.lab.example.com>
2022-07-02 00:50:09 -0400

    Add signin script.sh

---
+++ signin/si.sh
@@ -0,0 +1,8 @@
+source /usr/local/etc/ocp4.config
+
+export U=$RHT_OCP4_DEV_USER
+export P=$RHT_OCP4_DEV_PASSWORD
+export D=$RHT_OCP4_WILDCARD_DOMAIN
+export M=$RHT_OCP4_MASTER_API
+
+oc login -u $RHT_OCP4_DEV_USER -p $RHT_OCP4_DEV_PASSWORD $RHT_OCP4_MASTER_API
